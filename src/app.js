df80b9a14d1e4aefeb6ece6ec5887e01b495ef1b
KZN245 <kevin.shin@capitalone.com>
2016-09-27 13:12:40 -0400

    Added props example

--- src/app.js
+++ src/app.js
@@ -4,11 +4,37 @@ var Navbar = require('./navbar.js');
 
 var Hello = React.createClass({
 
+  getInitialState: function() {
+    return {
+      Home: true,
+      Calendar: false,
+      Annoucements: false,
+      Committees: false
+    }
+  },
+
   render: function() {
     return <div>
-      <Navbar />
+      <Navbar
+        navClick={this.handleChildClick}
+        PHome={this.state.Home}
+        PCalendar={this.state.Calendar}
+        PAnnoucements={this.state.Annoucements}
+        PCommittees={this.state.Committees} />
     </div>
+  },
+
+  handleChildClick: function(tab) {
+    var update = {
+      Home: false,
+      Calendar: false,
+      Annoucements: false,
+      Committees: false
+    }
+    update[tab] = true;
+    this.setState(update);
   }
+
 });
 
 var element = React.createElement(Hello, {});
--- src/navbar.js
+++ src/navbar.js
@@ -1,26 +1,17 @@
 var React = require('react');
 
 var Navbar = React.createClass ({
-  getInitialState: function() {
-    return {
-      Home: true,
-      Calendar: false,
-      Annoucements: false,
-      Committees: false
-    }
-  },
-
   render: function() {
     return <div>
 
     <div className="ui secondary menu">
-      <a className={"item"+(this.state.Home==true ? ' active' : '')}
-         onClick={this.handleClick.bind(this,"Home")} >Home</a>
-      <a className={"item"+(this.state.Calendar==true  ? ' active' : '')}
-         onClick={this.handleClick.bind(this,"Calendar")} >Calendar</a>
-      <a className={"item"+(this.state.Annoucements==true  ? ' active' : '')}
-         onClick={this.handleClick.bind(this,"Annoucements")} >Annoucements</a>
-      <a className={"item"+(this.state.Committees==true  ? ' active' : '')}
+      <a className={"item"+(this.props.PHome==true ? ' active' : '')}
+        onClick={this.handleClick.bind(this,"Home")} >Home</a>
+      <a className={"item"+(this.props.PCalendar==true  ? ' active' : '')}
+        onClick={this.handleClick.bind(this,"Calendar")} >Calendar</a>
+      <a className={"item"+(this.props.PAnnoucements==true  ? ' active' : '')}
+        onClick={this.handleClick.bind(this,"Annoucements")} >Annoucements</a>
+      <a className={"item"+(this.props.PCommittees==true  ? ' active' : '')}
         onClick={this.handleClick.bind(this,"Committees")}>Committees </a>
     </div>
 
@@ -28,14 +19,7 @@ var Navbar = React.createClass ({
   },
 
   handleClick: function(tab) {
-    var update = {
-      Home: false,
-      Calendar: false,
-      Annoucements: false,
-      Committees: false
-    };
-    update[tab] = true;
-    this.setState(update);
+    this.props.navClick(tab);
   }
 });
 
